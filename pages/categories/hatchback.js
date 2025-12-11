4801546a660d9d0f1841abc57a746a767193f824
Mahdi <mahdi1nap@gmail.com>
2025-12-11 21:23:24 +0330

    addcategories pages

---
+++ pages/categories/hatchback.js
@@ -0,0 +1,7 @@
+export default function Hatchback(){
+    return(
+        <>
+        hatchback
+        </>
+    )
+}
---
+++ pages/categories/sedan.js
@@ -0,0 +1,7 @@
+export default function Sedan(){
+    return(
+        <>
+        Sedan
+        </>
+    )
+}
